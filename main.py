e4c716a85bf1c028bf3208b28fcca42257393ae7
ifilatov <ivan@filatovz.ru>
2021-04-22 09:40:21 +0300

    add send file test

--- main.py
+++ main.py
@@ -1,26 +1,10 @@
-from telegram.client import Telegram
-from config import *
+from telethon import TelegramClient, events, sync
+import config
 
-tg = Telegram(
-    api_id=api_id,
-    api_hash=api_hash,
-    phone=phone,  # you can pass 'bot_token' instead
-    database_encryption_key=database_encryption_key,
-)
-tg.login()
+api_id = config.api_id
+api_hash = config.api_hash
 
-def new_message_handler(update):
-    # we want to process only text messages
-    message_content = update['message']['content'].get('text', {})
-    message_text = message_content.get('text', '').lower()
+client = TelegramClient(config.session_name, api_id, api_hash)
+client.start()
 
-    if message_text == 'ping':
-        chat_id = update['message']['chat_id']
-        print(f'Ping has been received from {chat_id}')
-        tg.send_message(
-            chat_id=chat_id,
-            text='pong',
-        )
-
-tg.add_message_handler(new_message_handler)
-tg.idle()  # you must call `stop` at the end of the script
+client.send_file(config.user_name, 'requirements.txt')
